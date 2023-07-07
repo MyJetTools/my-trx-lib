ae56d0764dc70b15d9b72b28fee472c84c842c9b
amigin <amigin@gmail.com>
2023-07-07 11:57:35 +0300

    Debugging

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 mod trx_address;
 mod trx_client;
-mod trx_model;
+pub mod trx_model;
 mod usdt_params_parse;
 
 pub use trx_address::*;
 pub use trx_client::*;
-pub use trx_model::*;
 pub use usdt_params_parse::*;
 
 pub const TRX_AMOUNT_MULTIPLIER: u64 = 1_000_000;
